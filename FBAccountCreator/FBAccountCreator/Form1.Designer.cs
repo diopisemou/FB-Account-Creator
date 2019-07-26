31c2281619ed3231c7bb95b7e0dba27e1838ad37
Lukas Zamyslicky <34404195+TheRealMajner@users.noreply.github.com>
2019-07-26 19:26:24 +0200

    Add files via upload

---
+++ FBAccountCreator/FBAccountCreator/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace FBAccountCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroSetControlBox1 = new MetroSet_UI.Controls.MetroSetControlBox();
+            this.metroSetButton1 = new MetroSet_UI.Controls.MetroSetButton();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.metroSetButton2 = new MetroSet_UI.Controls.MetroSetButton();
+            this.metroSetButton3 = new MetroSet_UI.Controls.MetroSetButton();
+            this.metroSetButton4 = new MetroSet_UI.Controls.MetroSetButton();
+            this.SuspendLayout();
+            // 
+            // metroSetControlBox1
+            // 
+            this.metroSetControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroSetControlBox1.CloseHoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.metroSetControlBox1.CloseHoverForeColor = System.Drawing.Color.White;
+            this.metroSetControlBox1.CloseNormalForeColor = System.Drawing.Color.Gray;
+            this.metroSetControlBox1.DisabledForeColor = System.Drawing.Color.DimGray;
+            this.metroSetControlBox1.Location = new System.Drawing.Point(561, 1);
+            this.metroSetControlBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.metroSetControlBox1.MaximizeBox = true;
+            this.metroSetControlBox1.MaximizeHoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.metroSetControlBox1.MaximizeHoverForeColor = System.Drawing.Color.Gray;
+            this.metroSetControlBox1.MaximizeNormalForeColor = System.Drawing.Color.Gray;
+            this.metroSetControlBox1.MinimizeBox = true;
+            this.metroSetControlBox1.MinimizeHoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.metroSetControlBox1.MinimizeHoverForeColor = System.Drawing.Color.Gray;
+            this.metroSetControlBox1.MinimizeNormalForeColor = System.Drawing.Color.Gray;
+            this.metroSetControlBox1.Name = "metroSetControlBox1";
+            this.metroSetControlBox1.Size = new System.Drawing.Size(100, 25);
+            this.metroSetControlBox1.Style = MetroSet_UI.Design.Style.Light;
+            this.metroSetControlBox1.StyleManager = null;
+            this.metroSetControlBox1.TabIndex = 0;
+            this.metroSetControlBox1.Text = "metroSetControlBox1";
+            this.metroSetControlBox1.ThemeAuthor = "Narwin";
+            this.metroSetControlBox1.ThemeName = "MetroLite";
+            // 
+            // metroSetButton1
+            // 
+            this.metroSetButton1.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.metroSetButton1.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.metroSetButton1.DisabledForeColor = System.Drawing.Color.Gray;
+            this.metroSetButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.metroSetButton1.HoverBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(207)))), ((int)(((byte)(255)))));
+            this.metroSetButton1.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(207)))), ((int)(((byte)(255)))));
+            this.metroSetButton1.HoverTextColor = System.Drawing.Color.White;
+            this.metroSetButton1.Location = new System.Drawing.Point(15, 338);
+            this.metroSetButton1.Name = "metroSetButton1";
+            this.metroSetButton1.NormalBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.metroSetButton1.NormalColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.metroSetButton1.NormalTextColor = System.Drawing.Color.White;
+            this.metroSetButton1.PressBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(147)))), ((int)(((byte)(195)))));
+            this.metroSetButton1.PressColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(147)))), ((int)(((byte)(195)))));
+            this.metroSetButton1.PressTextColor = System.Drawing.Color.White;
+            this.metroSetButton1.Size = new System.Drawing.Size(311, 34);
+            this.metroSetButton1.Style = MetroSet_UI.Design.Style.Dark;
+            this.metroSetButton1.StyleManager = null;
+            this.metroSetButton1.TabIndex = 1;
+            this.metroSetButton1.Text = "Create";
+            this.metroSetButton1.ThemeAuthor = "Narwin";
+            this.metroSetButton1.ThemeName = "MetroDark";
+            this.metroSetButton1.Click += new System.EventHandler(this.MetroSetButton1_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(15, 150);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 21);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(632, 182);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // metroSetButton2
+            // 
+            this.metroSetButton2.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.metroSetButton2.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.metroSetButton2.DisabledForeColor = System.Drawing.Color.Gray;
+            this.metroSetButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.metroSetButton2.HoverBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(207)))), ((int)(((byte)(255)))));
+            this.metroSetButton2.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(207)))), ((int)(((byte)(255)))));
+            this.metroSetButton2.HoverTextColor = System.Drawing.Color.White;
+            this.metroSetButton2.Location = new System.Drawing.Point(336, 338);
+            this.metroSetButton2.Name = "metroSetButton2";
+            this.metroSetButton2.NormalBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.metroSetButton2.NormalColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.metroSetButton2.NormalTextColor = System.Drawing.Color.White;
+            this.metroSetButton2.PressBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(147)))), ((int)(((byte)(195)))));
+            this.metroSetButton2.PressColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(147)))), ((int)(((byte)(195)))));
+            this.metroSetButton2.PressTextColor = System.Drawing.Color.White;
+            this.metroSetButton2.Size = new System.Drawing.Size(311, 34);
+            this.metroSetButton2.Style = MetroSet_UI.Design.Style.Dark;
+            this.metroSetButton2.StyleManager = null;
+            this.metroSetButton2.TabIndex = 5;
+            this.metroSetButton2.Text = "Clear IE Traces";
+            this.metroSetButton2.ThemeAuthor = "Narwin";
+            this.metroSetButton2.ThemeName = "MetroDark";
+            this.metroSetButton2.Click += new System.EventHandler(this.MetroSetButton2_Click);
+            // 
+            // metroSetButton3
+            // 
+            this.metroSetButton3.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.metroSetButton3.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.metroSetButton3.DisabledForeColor = System.Drawing.Color.Gray;
+            this.metroSetButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.metroSetButton3.HoverBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(207)))), ((int)(((byte)(255)))));
+            this.metroSetButton3.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(207)))), ((int)(((byte)(255)))));
+            this.metroSetButton3.HoverTextColor = System.Drawing.Color.White;
+            this.metroSetButton3.Location = new System.Drawing.Point(15, 378);
+            this.metroSetButton3.Name = "metroSetButton3";
+            this.metroSetButton3.NormalBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.metroSetButton3.NormalColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.metroSetButton3.NormalTextColor = System.Drawing.Color.White;
+            this.metroSetButton3.PressBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(147)))), ((int)(((byte)(195)))));
+            this.metroSetButton3.PressColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(147)))), ((int)(((byte)(195)))));
+            this.metroSetButton3.PressTextColor = System.Drawing.Color.White;
+            this.metroSetButton3.Size = new System.Drawing.Size(632, 34);
+            this.metroSetButton3.Style = MetroSet_UI.Design.Style.Dark;
+            this.metroSetButton3.StyleManager = null;
+            this.metroSetButton3.TabIndex = 6;
+            this.metroSetButton3.Text = "Verify";
+            this.metroSetButton3.ThemeAuthor = "Narwin";
+            this.metroSetButton3.ThemeName = "MetroDark";
+            this.metroSetButton3.Click += new System.EventHandler(this.MetroSetButton3_Click);
+            // 
+            // metroSetButton4
+            // 
+            this.metroSetButton4.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.metroSetButton4.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.metroSetButton4.DisabledForeColor = System.Drawing.Color.Gray;
+            this.metroSetButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.metroSetButton4.HoverBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(207)))), ((int)(((byte)(255)))));
+            this.metroSetButton4.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(207)))), ((int)(((byte)(255)))));
+            this.metroSetButton4.HoverTextColor = System.Drawing.Color.White;
+            this.metroSetButton4.Location = new System.Drawing.Point(0, 0);
+            this.metroSetButton4.Name = "metroSetButton4";
+            this.metroSetButton4.NormalBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.metroSetButton4.NormalColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.metroSetButton4.NormalTextColor = System.Drawing.Color.White;
+            this.metroSetButton4.PressBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(147)))), ((int)(((byte)(195)))));
+            this.metroSetButton4.PressColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(147)))), ((int)(((byte)(195)))));
+            this.metroSetButton4.PressTextColor = System.Drawing.Color.White;
+            this.metroSetButton4.Size = new System.Drawing.Size(0, 0);
+            this.metroSetButton4.Style = MetroSet_UI.Design.Style.Light;
+            this.metroSetButton4.StyleManager = null;
+            this.metroSetButton4.TabIndex = 0;
+            this.metroSetButton4.ThemeAuthor = "Narwin";
+            this.metroSetButton4.ThemeName = "MetroLite";
+            // 
+            // Form1
+            // 
+            this.AllowResize = false;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(662, 428);
+            this.Controls.Add(this.metroSetButton4);
+            this.Controls.Add(this.metroSetButton3);
+            this.Controls.Add(this.metroSetButton2);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.metroSetButton1);
+            this.Controls.Add(this.metroSetControlBox1);
+            this.Font = new System.Drawing.Font("Ink Free", 12.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.Padding = new System.Windows.Forms.Padding(12, 71, 12, 13);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Style = MetroSet_UI.Design.Style.Dark;
+            this.Text = "FBAccountCreator";
+            this.TextColor = System.Drawing.Color.White;
+            this.ThemeName = "MetroDark";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroSet_UI.Controls.MetroSetControlBox metroSetControlBox1;
+        private MetroSet_UI.Controls.MetroSetButton metroSetButton1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private MetroSet_UI.Controls.MetroSetButton metroSetButton2;
+        private MetroSet_UI.Controls.MetroSetButton metroSetButton3;
+        private MetroSet_UI.Controls.MetroSetButton metroSetButton4;
+    }
+}
+
---
+++ FBAccountCreator/FBAccountCreator/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroSet_UI.Forms;
+using System.Threading;
+
+namespace FBAccountCreator
+{
+    public partial class Form1 : MetroSetForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.facebook.com/");
+        }
+
+        private void MetroSetButton1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int length = 12;
+            var str = "";
+            for (var i = 0; i < length; i++)
+            {
+                str += ((char)(rnd.Next(1, 26) + 64)).ToString();
+            }
+
+            var random = new Random();
+            var list = new List<string> { "Kenneth", "James", "Micheal", "Freddie", "Joyce", "Kelly", "Marie", "Ted", "David", "Antonio", "Genevieve", "Janette", "Jennie", "Ron", "Jerald", "Kenneth", "Armando", "Robert", "Kelsey", "Joyce", "Alan", "Robin", "Samuel", "Patricia", "Jennie", "Grace", "Otis", "Antonio", "Jerald", "James", "Darrin", "Donald", "Frederick", "Emma", "Keith", "Kenneth" };
+            int index = random.Next(list.Count);
+
+            var list2 = new List<string> { "Mullins", "Smith", "Savage", "Reid", "bot", "Ramos", "Dibella", "White", "Karr", "Guidry", "Wheeler", "Land", "Day", "Clark", "Maynor", "Alderman", "Cox", "Shea", "Nichols", "Burt", "Portwood", "Sandova", "Wheeler", "Santos", "Durham", "Williams", "Acevedo", "Lloyd", "Malone", "Hayes", "Branch", "Silva", "Conrad", "Nicholson", "Mullins", "Hanson" };
+            int index2 = random.Next(list2.Count);
+
+            int birthday_day = rnd.Next(1, 28);
+            int birthday_month = rnd.Next(1, 12);
+            int birthday_year = rnd.Next(1970, 2001);
+
+            webBrowser1.Document.GetElementById("firstname").SetAttribute("value", list[index]);
+
+            webBrowser1.Document.GetElementById("lastname").SetAttribute("value", list2[index2]);
+
+            webBrowser1.Document.GetElementById("reg_email__").SetAttribute("value", str + "@domain.extension"); //over here you need fix or just create textbox and input mail info there
+
+            webBrowser1.Document.GetElementById("reg_email_confirmation__").SetAttribute("value", str + "@domain.extension");  //same as 2 lines above
+
+            webBrowser1.Document.GetElementById("reg_passwd__").SetAttribute("value", str);
+
+            webBrowser1.Document.GetElementById("birthday_day").SetAttribute("value", birthday_day.ToString());
+
+            webBrowser1.Document.GetElementById("birthday_month").SetAttribute("value", birthday_month.ToString());
+
+            webBrowser1.Document.GetElementById("birthday_year").SetAttribute("value", birthday_year.ToString());
+
+            webBrowser1.Document.GetElementsByTagName("input").GetElementsByName("sex")[0].InvokeMember("click"); //female
+
+            webBrowser1.Document.GetElementsByTagName("input").GetElementsByName("sex")[1].InvokeMember("click"); //male
+
+            webBrowser1.Document.GetElementById("websubmit").InvokeMember("Click");
+
+        }
+
+        private void MetroSetButton2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("CMD.exe", "/C RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 2");
+
+            //not needed
+        }
+
+        private void MetroSetButton3_Click(object sender, EventArgs e)
+        {
+            //associated with email code will redirect site after entering it currently bound to other button for manual activation to make it automatic you need fix/implement stuff for receive code from email and then move this line back to the button 1 function
+
+            webBrowser1.Document.GetElementById("confirm").InvokeMember("Click");
+        }
+
+    }
+}
